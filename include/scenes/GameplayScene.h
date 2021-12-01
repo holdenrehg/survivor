31e98497fb8460777aef6dfcdf0caa2178995a57
holdenrehg <holdenrehg@gmail.com>
2021-12-01 11:30:41 -0600

    [ADD] simple player animator class with a single idle animation

--- include/scenes/GameplayScene.h
+++ include/scenes/GameplayScene.h
@@ -5,17 +5,41 @@
 
 namespace Catcher::Survivor
 {
+    typedef enum PlayerAnimation { IDLE, WALK_UP, WALK_DOWN, WALK_LEFT, WALK_RIGHT } PlayerAnimation;
+
+    class PlayerAnimator
+    {
+        public:
+            PlayerAnimator();
+            void draw(int x, int y);
+            void nextFrame();
+            void setIdle();
+            void setWalkUp();
+            void setWalkDown();
+            void setWalkLeft();
+            void setWalkRight();
+        private:
+            Texture2D spriteSheet;
+            PlayerAnimation currentAnimation;
+            int currentFrame;
+            int currentFrameLimit;
+            Vector2 currentFrames[3];
+            int animationDelay;
+            long int lastUpdate;
+    };
+
     class GameplayScene : public Scene
     {
         public:
             GameplayScene();
             void handleInput(Game *game) override;
+            void update(Game *game) override;
             void draw(Game *game) override;
         private:
             Texture2D tileSprite;
-            Texture2D playerSprite;
             int playerSpeed;
             int playerX;
             int playerY;
+            PlayerAnimator playerAnimator;
     };
 }
--- src/scenes/GameplayScene.cpp
+++ src/scenes/GameplayScene.cpp
@@ -1,14 +1,109 @@
 #include "raylib.h"
 #include "include/colors.h"
+#include "include/engine/time.h"
 #include "include/engine/Game.h"
 #include "include/scenes/GameplayScene.h"
 
 using namespace Catcher::Survivor;
 
+PlayerAnimator::PlayerAnimator()
+{
+    spriteSheet = LoadTexture("resources/textures/character_sheet.png");
+    animationDelay = 500;
+    lastUpdate = timeSinceEpoch();
+    setIdle();
+}
+
+void PlayerAnimator::draw(int x, int y)
+{
+    Vector2 spritePosition = currentFrames[currentFrame];
+    Rectangle slice = { spritePosition.x, spritePosition.y, 16, 16 };
+    Vector2 position = { (float)x, (float)y };
+    DrawTextureRec(spriteSheet, slice, position, WHITE);
+}
+
+void PlayerAnimator::nextFrame()
+{
+    if(timeSinceEpoch() - lastUpdate > animationDelay)
+    {
+        currentFrame = (currentFrame + 1) % currentFrameLimit;
+        lastUpdate = timeSinceEpoch();
+    }
+}
+
+void PlayerAnimator::setIdle()
+{
+    if(currentAnimation != IDLE)
+    {
+        currentFrame = 0;
+        currentFrameLimit = 2;
+        currentAnimation = IDLE;
+    }
+
+    Vector2 frame0 = { 0, 0 };
+    currentFrames[0] = { frame0 };
+
+    Vector2 frame1 = { 16, 0 };
+    currentFrames[1] = { frame1 };
+}
+
+void PlayerAnimator::setWalkUp()
+{
+    if(currentAnimation != WALK_UP)
+    {
+        currentFrame = 0;
+        currentFrameLimit = 1;
+        currentAnimation = WALK_UP;
+    }
+
+    Vector2 frame0 = { 0, 32 };
+    currentFrames[0] = { frame0 };
+}
+
+void PlayerAnimator::setWalkDown()
+{
+    if(currentAnimation != WALK_DOWN)
+    {
+        currentFrame = 0;
+        currentFrameLimit = 1;
+        currentAnimation = WALK_DOWN;
+    }
+
+    Vector2 frame0 = { 0, 16 };
+    currentFrames[0] = { frame0 };
+}
+
+void PlayerAnimator::setWalkLeft()
+{
+    if(currentAnimation != WALK_LEFT)
+    {
+        currentFrame = 0;
+        currentFrameLimit = 1;
+        currentAnimation = WALK_LEFT;
+    }
+
+    Vector2 frame0 = { 0, 64 };
+    currentFrames[0] = { frame0 };
+}
+
+void PlayerAnimator::setWalkRight()
+{
+    if(currentAnimation != WALK_RIGHT)
+    {
+        currentFrame = 0;
+        currentFrameLimit = 1;
+        currentAnimation = WALK_RIGHT;
+    }
+
+    Vector2 frame0 = { 0, 48 };
+    currentFrames[0] = { frame0 };
+}
+
 GameplayScene::GameplayScene()
 {
+
     tileSprite = LoadTexture("resources/textures/tiles/tile_0000.png");
-    playerSprite = LoadTexture("resources/textures/sprites/character2.png");
+    playerAnimator = PlayerAnimator();
     playerSpeed = 1;
     playerX = 10;
     playerY = 10;
@@ -16,10 +111,37 @@ GameplayScene::GameplayScene()
 
 void GameplayScene::handleInput(Game *game)
 {
-    if(IsKeyDown(KEY_LEFT)) playerX -= playerSpeed;
-    if(IsKeyDown(KEY_RIGHT)) playerX += playerSpeed;
-    if(IsKeyDown(KEY_UP)) playerY -= playerSpeed;
-    if(IsKeyDown(KEY_DOWN)) playerY += playerSpeed;
+
+    if(IsKeyDown(KEY_LEFT))
+    {
+        playerAnimator.setWalkLeft();
+        playerX -= playerSpeed;
+    }
+    else if(IsKeyDown(KEY_RIGHT))
+    {
+        playerAnimator.setWalkRight();
+        playerX += playerSpeed;
+    }
+    else if(IsKeyDown(KEY_UP))
+    {
+        playerAnimator.setWalkUp();
+        playerY -= playerSpeed;
+    }
+    else if(IsKeyDown(KEY_DOWN))
+    {
+        playerAnimator.setWalkDown();
+        playerY += playerSpeed;
+    }
+    else
+    {
+        playerAnimator.setIdle();
+    }
+}
+
+void GameplayScene::update(Game *game)
+{
+
+    playerAnimator.nextFrame();
 }
 
 void GameplayScene::draw(Game *game)
@@ -40,5 +162,5 @@ void GameplayScene::draw(Game *game)
     }
 
     // Draw player sprite ------------------------------------------------------
-    DrawTexture(playerSprite, playerX, playerY, WHITE);
+    playerAnimator.draw(playerX, playerY);
 }
